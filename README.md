e08779f684a679724ea741d0343a12c4a425a758
James Campbell <james@private.tech>
2022-09-29 17:40:46 -0400

    added comments and updated readme

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 ### HOW
 
 1. Do `pipenv shell`
-2. Do `python3 src/merk.py 123456789` to validate that that number or string is not in the tree
+2. Do `python3 src/merk.py "ablacklistedimsi"` to validate that that number or string is not in the tree
+   1. Note that the double quotes are important, no funny business
+   2. ablacklistedimsi string data will validate True as a member, change to something else to get False
 3. Do `python3 src/treegen.py to show the example tree as a tree diagram using tkinter and matplotlib`
+
+### THE REST
+
+Who knows. The dude abides.
 
--- src/merk.py
+++ src/merk.py
@@ -1,18 +1,25 @@
 import merkletools  # type: ignore
 import hashlib
 
+# Generate merkletree object
 mt = merkletools.MerkleTools()
 
+# Generate some leaves
 hex_data = '05ae04314577b2783b4be98211d1b72476c59e9c413cfb2afa2f0c68e0d93911'
 print(f"Example hex data added to merkle tree as leaf directly: {hex_data}")
 list_data = ['Some text data', 'perhaps']
 print(f"Example list of values auto-added and auto-hashed strings added to tree as leaves for these values: {list_data}")
+
+# Add data to the merkle tree
 mt.add_leaf(hex_data)
 mt.add_leaf(list_data, True)
 mt.add_leaf("aaaaaa")
+
+# Make the tree now that you have all the values you need. Note you can generate a lot of blanks first
 mt.make_tree()
 root = mt.get_merkle_root()
-print(root)
+print(f"Root from the tree: {root}")
+
 # hash testing
 hash = hashlib.sha256(b'Some text data')
 print(hash.digest())
